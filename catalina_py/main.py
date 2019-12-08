b55cac82b57fbc0fb1b259b45a1d387cbdfedcee
sled30 <max-30ru@mail.ru>
2019-12-08 13:13:26 +0400

    read file

--- catalina_py/main.py
+++ catalina_py/main.py
@@ -1,11 +1,22 @@
 import os
+import time
 
-file_name = os.listdir(path = 'file/')
-print(file_name)
-
-def read_file():
-    pass
+def read_file(file):
+    with open('file/' + file,'r') as line:
+        string = line.read().split()
+        line.readline
+        print(string)
+    #    time.sleep(1)
 def parser_source():
     pass
 def save_source():
     pass
+def save_lor():
+    pass
+def work_sqlite():
+    pass
+
+
+file_list = os.listdir(path = 'file/')
+for file in file_list:
+    read_file(file)
