05b760d35c146f5640e0dd536d6dfc8f30b06cac
sled30 <max-30ru@mail.ru>
2019-12-31 00:28:32 +0400

    rewrite regxp from string

--- catalina_py/main.py
+++ catalina_py/main.py
@@ -1,4 +1,5 @@
 import os
+import sys
 import re
 import time
 
@@ -10,11 +11,13 @@ def read_file(file):
 def parser(string):
     source = re.search(r"(\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}\.\d{1,3}).{1,}для клиента (\d{11,12}).{1,}\[(.{1,})\]", string)
     if source:
+        return 1
         #print(source[1])
-        break
-    response = re.search(r"(\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}).\d{1,}.{1,}клиенту (\d{11,12}) отправлено: \{.{1,}failure\":(\d{1,}).{1,}\[(.{0,}\{\"error\":\"\w{1,}\"\}.{0,})\]")
-    if response:
-        print(response)
+    else:
+        response = re.search(r"(\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}).\d{1,}.{1,}клиенту (\d{11,12}) отправлено: \{.{1,}failure\":(\d{1,}).{1,}\[(.{0,}\{\"error\":\"\w{1,}\"\}.{0,})\]", string)
+        if response:
+            return 1
+
 
 def save_source():
     pass
