4b083740c702536092d02fe1f26bbec27084a1e3
sled30 <max-30ru@mail.ru>
2020-01-19 17:19:28 +0400

    create db

--- catalina_py/main.py
+++ catalina_py/main.py
@@ -17,9 +17,41 @@ def serialase_date(date):
     date = dateutil.parser.parse(date)
     return date.timestamp()
 def conn():
-    connect = sqlite3.connect(db)
+    connect = sqlite3.connect("db")
     return connect
+def create_db():
+    """ create structure db """
+    create_source = "CREATE TABLE IF NOT EXISTS `source` ( \
+	           `id` INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL , \
+            	  `phone` varchar(14) NOT NULL, `status` varchar(3) NOT NULL, \
+                  `time` varchar(20) NOT NULL, `ids` varchar(50) NOT NULL)"
+    create_ids = "CREATE TABLE IF NOT EXISTS `ids` ( \
+              	  `id` INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL , \
+              	  `phone` varchar(14) NOT NULL, `string_error` varchar(100) NOT NULL, \
+                  `secund` varchar(2) NULL, `status` varchar(2) NULL, \
+                  `1_error` varchar(100) NULL, `2_error` varchar(100) NULL, \
+                  `3_error` varchar(100) NULL, `4_error` varchar(100) NULL, \
+                  `5_error` varchar(100) NULL,  `6_error` varchar(100) NULL, \
+                  `7_error` varchar(100) NULL, `8_error` varchar(100) NULL, \
+                  `9_error` varchar(100) NULL, `10_error` varchar(100) NULL, \
+                  `11_error` varchar(100) NULL,  `12_error` varchar(100) NULL, \
+                  `13_error` varchar(100) NULL,  `14_error` varchar(100) NULL, \
+                  `15_error` varchar(100) NULL, `16_error` varchar(100) NULL, \
+                  `17_error` varchar(100) NULL, `18_error` varchar(100) NULL, \
+                  `19_error` varchar(100) NULL, `20_error` varchar(100) NULL, \
+                  `21_error` varchar(100) NULL, `22_error` varchar(100) NULL, \
+                  `23_error` varchar(100) NULL,  `24_error` varchar(100) NULL, \
+                  `25_error` varchar(100) NULL,  `26_error` varchar(100) NULL, \
+                  `27_error` varchar(100) NULL, `28_error` varchar(100) NULL, \
+                  `29_error` varchar(100) NULL, `30_error` varchar(100) NULL)"
 
+    connect = conn()
+#    connect = sqlite3.connect("db")
+    with connect:
+    #    conn = connect.cursor()
+        connect.execute(create_source)
+        connect.execute(create_ids)
+        connect.close
 def parser(string):
     """
     date.timestamp() возвращает таймштамп
@@ -49,7 +81,7 @@ def save_lor():
 def work_sqlite():
     pass
 
-
-file_list = os.listdir(path = 'file/')
-for file in file_list:
-    read_file(file)
+create_db()
+#file_list = os.listdir(path = 'file/')
+#for file in file_list:
+#    read_file(file)
