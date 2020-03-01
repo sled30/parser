3e0a71525a1549c31db3c6954410fdfd9ec143cb
sled30 <max-30ru@mail.ru>
2020-03-01 15:22:42 +0400

    b2b получаем список линков на объявления

--- avtobus_1/agregator.py
+++ avtobus_1/agregator.py
@@ -100,22 +100,29 @@ urn_adverts = parse_page_to_link(source_index_page)
 
 for urn_advert in urn_adverts:
     url_advert = uri + urn_advert
+    print(url_advert)
     page_advert = get_page(url_advert)
     #print(page_advert)
     title = get_title_advert(page_advert)
+    print(title)
 
     okpd = get_okpd_advert(page_advert)
+    print(okpd)
 
     eat = get_eat_advert(page_advert)
+    print(eat)
 
     provisioner = get_provisioner(page_advert)
+    print(provisioner)
 
     unit = get_unit(page_advert)
-    #print(unit)
+    print(unit)
 
     amount = get_amount(page_advert)
+    print(amount)
 
     cost = get_cost(page_advert)
+    print(cost)
 
     date = get_date_of_creation(page_advert)
 
---
+++ avtobus_1/b2b-center.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python
+from selenium.webdriver import Firefox
+# import requests
+# from requests import Response
+# from selenium import webdriver
+from bs4 import BeautifulSoup
+import re
+
+uri = "https://www.b2b-center.ru"
+urn = "/market/?f_keyword=автобус&searching=1&main_page_search=1&from=20#search-result"
+url = uri + urn
+
+
+def get_page(url):
+    webdriver = "/home/sled/work/coding/parser/avtobus_1/lib/"
+    driver = Firefox(webdriver)
+    driver.get(url)
+    data = driver.page_source
+    driver.close()
+
+    return data
+def get_urn_advert(page):
+    try:
+        urn = re.findall(r'<tr><td><a href="(\/market\/.{10,}\/)"', page)
+
+        return urn
+
+    except Exception as e:
+        print('get_urn_advert')
+        print(e)
+
+
+page = get_page(url)
+urn_advert = get_urn_advert(page)
+print(urn_advert)
