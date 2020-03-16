82a0f1ca4baf9d7401da9bc84cb0650f98df7159
sled30 <max-30ru@mail.ru>
2020-03-16 22:41:13 +0400

    ебаннуцо комент

--- avtobus_1/agregator.py
+++ avtobus_1/agregator.py
@@ -5,10 +5,13 @@ from selenium.webdriver import Firefox
 # from selenium import webdriver
 from bs4 import BeautifulSoup
 import re
+import time
 
 uri = "https://agregatoreat.ru"
-urn = "/purchases/new"
-url = uri + urn
+urn = "/purchases/new/page/"
+base_page = str(1)
+link = uri + urn
+url = link + base_page
 
 
 def get_page(url):
@@ -16,6 +19,7 @@ def get_page(url):
     driver = Firefox(webdriver)
     driver.get(url)
     data = driver.page_source
+#    time.sleep(5)
     driver.close()
 
     return data
@@ -28,11 +32,148 @@ def parse_first_page(page):
         print('parse_first_page')
         print(e)
 
-def parse_advisor(url):
-    pass
+def get_max_num_page(date):
+    try:
+
+        max_num_page = re.search(r'item cursor-pointer px13 last\">(\d{1,3})', date)
+
+        return max_num_page[1]
+
+    except Exception as e:
+        print('get_max_num_page(date)')
+        print(e)
+
+def check_need_info(title):
+    try:
+        find = re.search(r'автобусы', title)
+        if find:
+            find = re.search(r'ремонтное', string)
+            if find is None:
+                return 1
+        find = re.search(r'перевозка', title)
+        if find:
+            find = re.search(r'машины', string)
+            if find is None:
+                return 1
+        find = re.search(r'транспорное', title)
+        if find:
+            find = re.search(r'спецтехники', string)
+            if find is None:
+                return 1
+        find = re.search(r'транспорные' , title)
+        if find:
+            find = re.search(r'продажа', string)
+            if find is None:
+                return 1
+        find = re.search(r'автобусов' , title)
+        if find:
+            find = re.search(r'специальный транспорт', string)
+            if find is None:
+                return 1
+        find = re.search(r'микроавтобусы' , title)
+        if find:
+            find = re.search(r'грузов', string)
+            if find is None:
+                return 1
+        find = re.search(r'транспортное сопровождение' , title)
+        if find:
+            find = re.search(r'промышленнных', string)
+            if find is None:
+                return 1
+        find = re.search(r'транспортное обслуживание' , title)
+        if find:
+            find = re.search(r'отходов', string)
+            if find is None:
+                return 1
+        find = re.search(r'транспорных услуг' , title)
+        if find:
+            find = re.search(r'страхование', string)
+            if find is None:
+                return 1
+        find = re.search(r'транспортного средства' , title)
+        if find:
+            find = re.search(r'осмотр', string)
+            if find is None:
+                return 1
+        find = re.search(r'микроавтобусов' , title)
+        if find:
+            find = re.search(r'техническое обслуживание', string)
+            if find is None:
+                return 1
+        find = re.search(r'транспортировка' , title)
+        if find:
+            find = re.search(r'оборудование', string)
+            if find is None:
+                return 1
+        find = re.search(r'перевозка сотрудников' , title)
+        if find:
+            find = re.search(r'технологическим', string)
+            if find is None:
+                return 1
+        find = re.search(r'регулярные перевозки' , title)
+        if find:
+            find = re.search(r'грузоперевозки', string)
+            if find is None:
+                return 1
+        find = re.search(r'транспортных средств' , title)
+        if find:
+            return 1
+
+        find = re.search(r'доставка сотрудников' , title)
+        if find:
+            return 1
+
+        find = re.search(r'детей' , title)
+        if find:
+            return 1
+
+        find = re.search(r'оферта' , title)
+        if find:
+            return 1
+
+        find = re.search(r'цветов' , title)
+        if find:
+            return 1
+
+    except Exception as e:
+        print('Error check_need_info(title) ')
+        print(e)
+#    status = None
+
+
+
+def parse_date(source_index_page, link):
+    max_num = get_max_num_page(source_index_page)
+    first_date = parse_first_page(source_index_page)
+    for date in first_date:
+        status = check_need_info(date[4])
+        if status is not None:
+            print('ебагуццооо')
+        print(date[4])
+        print(status)
+
+
+
+
+
+#############################################################################################################################################################
+#
+#
+#############################################################################################################################################################
+#
+#
+#############################################################################################################################################################
+
+
 
 source_index_page = get_page(url)
-first_date = parse_first_page(source_index_page)
+if source_index_page:
+    parse_date(source_index_page, link)
 
-for date in first_date:
-    print(date[4])
+
+
+
+
+#for date in first_date:
+#    parse_advisor(uri+date[1])
+#    break
