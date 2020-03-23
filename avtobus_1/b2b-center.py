0d27dffa6c002f564c0daac88b83c6ba40dfe7b2
sled30 <max-30ru@mail.ru>
2020-03-23 19:38:50 +0400

    check report b2b

--- avtobus_1/b2b-center.py
+++ avtobus_1/b2b-center.py
@@ -50,13 +50,22 @@ def b2b_load_general(url, uri):
 
 def b2b():
     uri = "https://www.b2b-center.ru"
-    urn = "/market/?f_keyword=автобус&searching=1&company_type=2&price_currency=0&date=1&trade=all#search-result"
+    find_string = commun.find_string()
+    for value in find_string:
+        urn = "/market/?f_keyword=" + value + "&searching=1&company_type=2&price_currency=0&date=1&trade=all#search-result"
+        url = uri + urn
+        b2b_load_general(url, uri)
+        page = commun.get_page(url)
+        max_page = max_num_page(page)
+        if max_page is not None:
+            print('хер знает что делать')
+    #urn = "/market/?f_keyword=автобус&searching=1&company_type=2&price_currency=0&date=1&trade=all#search-result"
     url = uri + urn
-    b2b_load_general(url, uri)
-    page = commun.get_page(url)
-    max_page = max_num_page(page)
-    if max_page is not None:
-        print('хер знает что делать')
+#    b2b_load_general(url, uri)
+#    page = commun.get_page(url)
+#    max_page = max_num_page(page)
+#    if max_page is not None:
+#        print('хер знает что делать')
 
 
 
@@ -87,33 +96,8 @@ def b2b():
 def main():
     pass
 
-#    #https://www.b2b-center.ru/market/?f_keyword=%D0%B0%D0%B2%D1%82%D0%BE%D0%B1%D1%83%D1%81&searching=1&company_type=2&price_currency=0&date=1&trade=buy&lot_type=0#search-result
-#    uri = "https://www.b2b-center.ru"
-#    urn = "/market/?f_keyword=автобус&searching=1&company_type=2&price_currency=0&date=1&trade=all#search-result"
-#    url = uri + urn
-#    page = commun.get_page(url)
-#    dates = parse_global_info(page)
-#    max_page = max_num_page(page)
-#    print(max_page)
-#
-#    for date in dates:
-#        check_ferst = commun.check_need_info(date[1])
-#        check_second = commun.check_need_info(date[2])
-#
-#        if check_ferst or check_second:
-#            url = uri + date[0]
-#            advert_page = commun.get_page(url)
-#            region = get_region(advert_page)
-#            date = (date[1], date[2], date[3], date[4], date[5], region, url)
-#            status = db.check_save_b2b(date[0])
-#            if status == True:
-#                db.save_b2b(date)
-
-
-
-
-#main()
 b2b()
+
 #<a href=\"(\/market\/[a-z-0-9]{3,}\/tender-\d{7}\/)\"[ a-z=\"-]{10,38}>([^<]{10,})<[^>]{10,39}>([^<]{4,})[^>].{42,}\"visited\">([^<]{3,})<.{27}(\d{2}.\d{2}.\d{4} \d{2}:\d{2}).{24}(\d{2}.\d{2}.\d{4} \d{2}:\d{2})
 ##
 #a title=\"\d{1,5} - (\d{1,5})\" href=\"\/market\/\?from=\d{1,5}\">\d{1,5}</a>\n  #стараницы
--- avtobus_1/db.py
+++ avtobus_1/db.py
@@ -20,7 +20,7 @@ def create_db_agregator():
                   `end_advert_date` varchar(50) NOT NULL, \
                   `region` varchar(50) NOT NULL, \
                   `date_of_conclusion` varchar(50) NOT NULL, \
-                  `url` varchar(50) NOT NULL) "
+                  `url` varchar(50) NOT NULL, `actual` varchar(50) NOT NULL) "
 
 
     create_source_index = "CREATE INDEX `number_advert` ON `agregator` (`number_advert`)"
@@ -33,6 +33,67 @@ def create_db_agregator():
     #    connect.execute(create_ids_index)
         connect.close
         return True
+
+def create_db_b2b():
+    """ create structure db """
+
+
+    create_source = "CREATE TABLE IF NOT EXISTS `b2b` ( \
+	           `id` INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL , \
+                  `number_advert` varchar(20) NOT NULL, \
+            	  `advert_text` varchar(20) NULL,  \
+                  `short_text_advert` varchar(50), \
+                  `name_organisation` varchar(50) NOT NULL, \
+                  `long_text_advert` varchar(50) NULL,  \
+                  `advert_type` varchar(20), \
+                  `createtion_date` varchar(50) NOT NULL, \
+                  `end_advert_date` varchar(50) NOT NULL, \
+                  `region` varchar(50) NOT NULL, \
+                  `status` varchar(2), \
+                  `date_of_conclusion` varchar(50), \
+                  `url` varchar(50) NOT NULL, `actual` varchar(50)) "
+
+
+    create_source_index = "CREATE INDEX `number_b2b` ON `b2b` (`number_advert`)"
+
+    connect = conn()
+    with connect:
+        connect.execute(create_source)
+
+        connect.execute(create_source_index)
+
+        connect.close
+        return True
+def save_b2b(date):
+    try:
+
+        sql = "INSERT INTO b2b (number_advert, \
+                                      advert_text, \
+                                      name_organisation, \
+                                      createtion_date, \
+                                      end_advert_date, \
+                                      actual, \
+                                      region, \
+                                      url) \
+                                      VALUES (?, ?, ?, ?, ?, ?, ?, ?)"
+
+                                    #  ('Мониторинг рынка цен № 1473345 \n', +++ number_advert date[0]
+                                     #  'запчасти для ремонта ', +++++ advert_text  date[1]
+                                     #  'ООО "ЛЕНИНГРАДСКАЯ АЭС-АВТО"', ++++ name_organisation [2]
+                                      # '17.03.2020 13:41', ++++ createtion_date [3]
+                                     #  '26.03.2020 23:59', ++++++ end_advert_date [4]
+                                    #   Не найдено', ++++++ status [5]
+                                     #  'https://www.b2b-center.ru/market/zapchasti-dlia-remonta-avtobusov-paz/tender-1473345/') ++++ url [6]
+
+
+        connect = conn()
+        cur = connect.cursor()
+        with connect:
+            cur.execute(sql, (date[0], date[1], date[2], date[3], date[4], '1', date[5], date[6]))
+        connect.close
+    except Exception as e:
+        print(e)
+
 
 def get_agregator():
     """   """
@@ -57,12 +118,55 @@ def get_agregator():
         cur.execute(sql)
         rows = cur.fetchall()
         return rows
-#        if len_rows == 0:
-#            return True
-#        else:
-#             return False
+        connect.close
+
+    except Exception as e:
+        print(e)
+
+def get_b2b():
+    """   """
+
+    try:
+        sql = "SELECT `number_advert`, \
+                      `advert_text`, \
+                      `name_organisation`, \
+                      `createtion_date`, \
+                      `end_advert_date`, \
+                      `region`, \
+                      `url`, \
+                      `actual` \
+                      from b2b"
+
+
+    #    sql = "select `number_advert` from agregator where number_advert='{}'".format(number_advert)
+        connect = conn()
+        cur = connect.cursor()
+        cur.execute(sql)
+        rows = cur.fetchall()
+        return rows
+        connect.close
+
+    except Exception as e:
+        print(e)
+
+def check_save_b2b(number_advert):
+
+    try:
+        sql = "select `number_advert` from b2b where number_advert='{}'".format(number_advert)
+        connect = conn()
+        cur = connect.cursor()
+        cur.execute(sql)
+        rows = cur.fetchall()
+        len_rows = len(rows)
+        if len_rows == 0:
+            return True
+        else:
+             return False
 
         connect.close
 
     except Exception as e:
         print(e)
+
+def main():
+    pass
